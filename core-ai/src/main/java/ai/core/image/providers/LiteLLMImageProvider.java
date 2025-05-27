835d7718391058acbb9c26a7a3ce42fe01010884
Stephen <demobin@163.com>
2025-05-27 15:35:34 +0800

    delete litellm and huggingface library

--- core-ai/src/main/java/ai/core/image/providers/LiteLLMImageProvider.java
+++ core-ai/src/main/java/ai/core/image/providers/LiteLLMImageProvider.java
@@ -3,27 +3,15 @@ package ai.core.image.providers;
 import ai.core.image.ImageProvider;
 import ai.core.image.providers.inner.GenerateImageRequest;
 import ai.core.image.providers.inner.GenerateImageResponse;
-import ai.core.litellm.LiteLLMService;
-import ai.core.litellm.image.CreateImageAJAXRequest;
-import core.framework.inject.Inject;
 
 
 /**
  * @author stephen
  */
 public class LiteLLMImageProvider implements ImageProvider {
-    @Inject
-    LiteLLMService liteLLMService;
 
     @Override
     public GenerateImageResponse generateImage(GenerateImageRequest request) {
-        var rsp = liteLLMService.generateImage(toGenerateApiRequest(request));
-        return new GenerateImageResponse(rsp.generatedDatas.getFirst().revisedPrompt, rsp.generatedDatas.getFirst().url);
-    }
-
-    private CreateImageAJAXRequest toGenerateApiRequest(GenerateImageRequest request) {
-        var apiReq = new CreateImageAJAXRequest();
-        apiReq.prompt = request.prompt();
-        return apiReq;
+        return null;
     }
 }
--- core-ai/src/main/java/ai/core/llm/providers/inner/FinishReason.java
+++ core-ai/src/main/java/ai/core/llm/providers/inner/FinishReason.java
@@ -1,9 +1,13 @@
 package ai.core.llm.providers.inner;
 
+import core.framework.api.json.Property;
+
 /**
  * @author stephen
  */
 public enum FinishReason {
+    @Property(name = "stop")
     STOP,
+    @Property(name = "tool_calls")
     TOOL_CALLS
 }
