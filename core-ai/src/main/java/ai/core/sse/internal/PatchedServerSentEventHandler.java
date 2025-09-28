966d49144d9ae1e542bb3bf3dadce64f7a64b4d2
Stephen <demobin@163.com>
2025-09-28 15:48:25 +0800

    fix aks gw health check npe

--- core-ai/src/main/java/ai/core/sse/internal/PatchedServerSentEventHandler.java
+++ core-ai/src/main/java/ai/core/sse/internal/PatchedServerSentEventHandler.java
@@ -52,6 +52,7 @@ public class PatchedServerSentEventHandler extends ServerSentEventHandler {
 
     @Override
     public boolean check(HttpString method, String path, HeaderMap headers) {
+        if (headers == null || headers.getFirst(Headers.ACCEPT) == null) return false; // aks gw health check use / and do not have accept headers, just return false
         return headers.getFirst(Headers.ACCEPT).contains("text/event-stream")
             && supports.containsKey(key(method.toString(), path));
     }
