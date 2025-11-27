0979eece65c2e8d189d698b0a1eea23f747e4ca8
Cyril <cyriltang@chancetop.com>
2025-11-27 18:28:21 +0800

    add response format schema test

---
+++ core-ai/src/test/java/ai/core/responseschema/LLMResponseSchemaTest.java
@@ -0,0 +1,17 @@
+package ai.core.responseschema;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author cyril
+ */
+public class LLMResponseSchemaTest {
+    @Test
+    void test() {
+
+        // Create CaptionImageRequest with structured extraction schema
+//        ResponseFormat responseFormat = ResponseFormat.of(MenuData.class);
+
+
+    }
+}
