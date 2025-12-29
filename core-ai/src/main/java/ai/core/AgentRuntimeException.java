2a0a843de9d41cbe033b7e4b982eaab24857aa3b
Stephen <demobin@163.com>
2025-12-29 11:50:28 +0800

    fix exception serial check

--- core-ai/src/main/java/ai/core/AgentRuntimeException.java
+++ core-ai/src/main/java/ai/core/AgentRuntimeException.java
@@ -2,10 +2,15 @@ package ai.core;
 
 import core.framework.log.ErrorCode;
 
+import java.io.Serial;
+
 /**
  * @author stephen
  */
 public class AgentRuntimeException extends RuntimeException implements ErrorCode {
+    @Serial
+    private static final long serialVersionUID = 8426481236734448674L;
+
     private final String errorCode;
 
     public AgentRuntimeException(String errorCode, String message) {
