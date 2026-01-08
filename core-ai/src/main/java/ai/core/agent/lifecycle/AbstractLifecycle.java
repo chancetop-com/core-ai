21c232ce15535b9e85e10c63568d6d6bc67e1a24
xander <canqiangxu@yeah.net>
2026-01-08 10:42:00 +0800

    remove useless comment

--- core-ai/src/main/java/ai/core/agent/lifecycle/AbstractLifecycle.java
+++ core-ai/src/main/java/ai/core/agent/lifecycle/AbstractLifecycle.java
@@ -12,25 +12,12 @@ import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * author: lim chen
- * date: 2025/11/10
- * description: AbstractLifecycle, control subclass action
  */
 public abstract class AbstractLifecycle {
-    /**
-     * at build stage
-     * beforeAgentBuild  Do something
-     *
-     * @param agentBuilder agentBuilder
-     */
     public void beforeAgentBuild(AgentBuilder agentBuilder) {
 
     }
-    /**
-     * at build stage
-     * afterAgentBuild Do something
-     *
-     * @param agent agentBuilder
-     */
+
     public void afterAgentBuild(Agent agent) {
 
     }
@@ -42,35 +29,15 @@ public abstract class AbstractLifecycle {
     public void afterModel(CompletionRequest completionRequest, CompletionResponse completionResponse, ExecutionContext executionContext) {
 
     }
-    /**
-     * at runtime stage
-     * beforeAgentRun Do something
-     *
-     * @param query            user query
-     * @param executionContext agen runtime context
-     */
+
     public void beforeAgentRun(AtomicReference<String> query, ExecutionContext executionContext) {
 
     }
-    /**
-     * at runtime stage
-     * afterAgentRun Do something
-     *
-     * @param query            user query
-     * @param result           agent run result(str)
-     * @param executionContext agen runtime context
-     */
+
     public void afterAgentRun(String query, AtomicReference<String> result, ExecutionContext executionContext) {
 
     }
-    /**
-     * at runtime stage
-     * afterAgentFailed Do something
-     *
-     * @param query            user query
-     * @param executionContext agen runtime context
-     * @param exception        ex
-     */
+
     public void afterAgentFailed(String query, ExecutionContext executionContext, Exception exception) {
 
     }
