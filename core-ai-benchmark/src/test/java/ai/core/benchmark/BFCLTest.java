f4074ca0d226cb45e645915a6754ce5e2b6694aa
Lim Chen <limchen@chancetop.com>
2026-01-13 17:04:32 +0800

    complete  write todos benchmark

--- core-ai-benchmark/src/test/java/ai/core/benchmark/BFCLTest.java
+++ core-ai-benchmark/src/test/java/ai/core/benchmark/BFCLTest.java
@@ -3,6 +3,7 @@ package ai.core.benchmark;
 import ai.core.benchmark.common.BFCLCategory;
 import ai.core.benchmark.evaluator.BFCLEvaluator;
 import ai.core.benchmark.inference.BFCLInferenceFCHandle;
+import ai.core.benchmark.inference.BFCLInferencePlanHandle;
 import ai.core.llm.LLMProviders;
 import core.framework.inject.Inject;
 import org.junit.jupiter.api.Disabled;
@@ -31,4 +32,15 @@ class BFCLTest extends IntegrationTest {
         eval.eval(BFCLCategory.LIVE, List.of());
 
     }
+    @Test
+    void test3() {
+        var eval = new BFCLEvaluator(new BFCLInferencePlanHandle(llmProviders.getProvider()), "gpt-5-nano-2025-08-07-FC");
+//        eval.eval(BFCLCategory.NON_LIVE, List.of("parallel_0"));
+//        eval.eval(BFCLCategory.LIVE, List.of("live_simple_61-29-1"));
+//        eval.eval(BFCLCategory.LIVE, List.of());
+        eval.eval(BFCLCategory.NON_LIVE, List.of());
+
+    }
+
+
 }
