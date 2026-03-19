f45989cb4309d75bd0a8a49ee496b839be0436a5
Stephen <demobin@163.com>
2026-03-19 18:37:26 +0800

    fix check

--- core-ai-api/src/main/java/ai/core/api/server/session/LoadToolsRequest.java
+++ core-ai-api/src/main/java/ai/core/api/server/session/LoadToolsRequest.java
@@ -1,10 +1,15 @@
 package ai.core.api.server.session;
 
+import core.framework.api.json.Property;
+import core.framework.api.validate.NotNull;
+
 import java.util.List;
 
 /**
  * @author stephen
  */
 public class LoadToolsRequest {
-    public List<String> toolIds;
+    @NotNull
+    @Property(name = "tool_ids")
+    public List<String> toolIds = List.of();
 }
--- core-ai-api/src/main/java/ai/core/api/server/session/LoadToolsResponse.java
+++ core-ai-api/src/main/java/ai/core/api/server/session/LoadToolsResponse.java
@@ -1,10 +1,13 @@
 package ai.core.api.server.session;
 
+import core.framework.api.json.Property;
+
 import java.util.List;
 
 /**
  * @author stephen
  */
 public class LoadToolsResponse {
+    @Property(name = "loaded_tools")
     public List<String> loadedTools;
 }
