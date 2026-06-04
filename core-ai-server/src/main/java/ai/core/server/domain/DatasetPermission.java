b3ae76ad3f11ff3b2a1dc54d69f70971e3d3f754
Stephen <demobin@163.com>
2026-06-04 15:20:33 +0800

    dataset with permission

--- core-ai-server/src/main/java/ai/core/server/domain/DatasetPermission.java
+++ core-ai-server/src/main/java/ai/core/server/domain/DatasetPermission.java
@@ -1,10 +1,15 @@
 package ai.core.server.domain;
 
+import core.framework.mongo.MongoEnumValue;
+
 /**
  * @author stephen
  */
 public enum DatasetPermission {
+    @MongoEnumValue("READ")
     READ,
+    @MongoEnumValue("WRITE")
     WRITE,
+    @MongoEnumValue("FULL")
     FULL
 }
